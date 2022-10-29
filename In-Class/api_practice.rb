fb827928c8ff50a2a792dfa99e2cd4ae216aeac2
Julia Grimes <julia.s.grimes@gmail.com>
2022-10-28 20:38:44 -0400

    practice accessing JSON info from quote generating API and practice using TTY-PROMPT gem

---
+++ In-Class/api_practice.rb
@@ -0,0 +1,19 @@
+require "http"
+require "tty-prompt"
+
+prompt = TTY::Prompt.new
+
+continue = true
+while continue == true
+  response = HTTP.get("https://api.fisenko.net/v1/quotes/en/random")
+  quote = response.parse(:json)
+  text = quote["text"]
+  name = quote["author"]["name"]
+  answer = prompt.select("Generate inspiring quote?", %w(Yes No))
+  if answer == "Yes"
+    puts text
+    puts "-#{name}"
+  else
+    continue = false
+  end
+end
